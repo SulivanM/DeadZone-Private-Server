e32dd671ec7a4e8422ef0f6cc8327224cde51798
glennhenry <glennhenrybdg05@gmail.com>
2025-11-06 18:35:49 +0700

    Delete code comments

--- src/main/kotlin/server/handler/save/survivor/response/PlayerCustomResponse.kt
+++ src/main/kotlin/server/handler/save/survivor/response/PlayerCustomResponse.kt
@@ -3,21 +3,14 @@ package server.handler.save.survivor.response
 import server.handler.save.BaseResponse
 import kotlinx.serialization.Serializable
 
-/**
- * Any update to player's leader, such as create leader, respec
- */
 @Serializable
 data class PlayerCustomResponse(
-    // only if error
+    
     val error: String? = null,
 
-    // list of string which is AttributesConstants
-    // likely used when leader respec skill
     val attributes: List<String> = emptyList(),
 
-    // likely used when respec and there is extra level point??
     val levelPts: Int? = null,
 
-    // base64 string, likely change name cooldown
     val cooldown: String? = null,
 ): BaseResponse()
